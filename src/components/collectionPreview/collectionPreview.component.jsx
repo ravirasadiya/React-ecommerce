10fcd62335bdd09f63513f0f63e6dcc1d718d0e1
Ravi Rasadiya <ravirasadiya1994@gmail.com>
2020-08-31 23:26:57 +0530

    collectionPreview Added

--- src/components/collectionPreview/collectionPreview.component.jsx
+++ src/components/collectionPreview/collectionPreview.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collectionItem.component';
+import './collectionPreview.style.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
@@ -6,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherProps }) => (
+          <CollectionItem key={id} {...otherProps} />
         ))}
     </div>
   </div>
